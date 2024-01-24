1804c2b43a83c362e805f7a56853bfeda6531d69
Niko Niemelä <nokka200@gmail.com>
2024-01-24 18:20:17 +0200

    First commit

---
+++ PalindromDetector.js
@@ -0,0 +1,34 @@
+"use strict";
+
+class PalindromDetector {
+
+    #input;
+
+    constructor() {
+        this.#input = require("readline-sync");
+    }
+
+    // Metodi ottaa käyttäjältä syötteen, muuttaa sen pieniksi kirjaimiksi ja kääntää sanan toisinpäin.
+    // lopuksi testaa onko sana palidromi eli onko se sama kuin käännettynä.
+    isPalindrom() {
+        let userInput = this.#input.question("Kirjoita sana: ").toLowerCase();
+        let userInputReverse = this.#reverseWord(userInput);
+        return userInputReverse === userInput ? true: false;
+    }
+
+    #reverseWord(word) {
+        let wordInReverse = new Array();
+
+        for (let i = word.length - 1 ; i >= 0; i--) {
+            wordInReverse.push(word[i]);
+        }
+        return wordInReverse.join("");
+    }
+    
+}
+
+let detector = new PalindromDetector();
+let result = new String();
+
+result = detector.isPalindrom();
+console.log(result);
---
+++ README.md
@@ -0,0 +1,12 @@
+Tehtävä anto
+
+Tee sovellus, joka kysyy syötettävän sanan (merkkijono).
+Sovellus testaa onko sana palindromi ts. sana on sama toisinpäin.
+Esim. sana saippuakauppias on palindromi.
+Luo ratkaisu siten että et käytä valmiita funktioita vaan teet oman algoritmin.
+
+Tehtävässä ei saa köyttää valmiita javascript funktioita joilla voisi kääntää merkkijonon (vaan merkkijonon kääntö on tehtävä itse silmukassa).
+
+Muuta edellinen ratkaisu funktioksi joka palauttaa boolean arvon true, mikäli funktion parametrina oleva sana on palindromi (jos ei, niin palautetaan false).
+
+Kutsu funktiota.
